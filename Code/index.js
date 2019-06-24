a0e0e0ac6c1cd3fb4b83102f5a17ab418ae56755
CTM-development <45259971+CTM-development@users.noreply.github.com>
2019-06-24 23:04:38 +0200

    basic dot chart and some failed .append() tests

--- Code/index.js
+++ Code/index.js
@@ -1,10 +1,17 @@
-var dataSet = [];
+
 var files = ['http://it2wi1.if-lab.de/rest/mpr_fall1'];
 var promises = [];
+
+var newSvg = d3.select('body').append('svg')
+    .attr('width', 200)
+    .attr('height', 200);
+
+d3.select('#header_div').append('p').text('hallo')
 
 files.forEach(function (files) {
     promises.push(d3.json(files))
 });
+
 
 Promise.all(promises).then((result) => receiveData(result), function (error) {
     console.log('Something went wrong!!' + error);
@@ -12,9 +19,26 @@ Promise.all(promises).then((result) => receiveData(result), function (error) {
 
 function receiveData(data) {
     console.log('data was received succefuly');
-    visData(data);
+    visualizeData(data);
 };
 
-function visData(data){
-    
+function visualizeData(data) {
+    console.log(data);
+    console.log(data[0][12]);
+
+    var canvas = d3.select('#svg_canvas')
+        .selectAll('g')
+        .data(data[0])
+        .enter()
+        .append('g')
+        .attr('class', 'node')
+        .attr('transform', function (d) {
+            return ('translate(' + (data[0].indexOf(d) * 1200 / 310 + 10) + ',' + ((600 - 0.5 * d.werte.Rpm) + 10) + ')');
+        })
+
+    canvas.append('circle')
+        .attr('r', 2)
 };
+
+
+
