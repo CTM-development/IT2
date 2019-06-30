a025cd89ec4bae262732c8fdc891b738c2c49de7
CTM-development <45259971+CTM-development@users.noreply.github.com>
2019-06-30 14:42:10 +0200

    empty files for further work

--- Code/index.js
+++ Code/index.js
@@ -1,5 +1,5 @@
 //save the url links to the data download here
-var files = ['http://it2wi1.if-lab.de/rest/mpr_fall1'];
+var files = ['http://it2wi1.if-lab.de/rest/mpr_fall1', 'http://it2wi1.if-lab.de/rest/mpr_fall2', 'http://it2wi1.if-lab.de/rest/mpr_fall3', 'http://it2wi1.if-lab.de/rest/mpr_fall4'];
 var promises = [];
 
 //set base val variables for later use
@@ -13,19 +13,29 @@ var y = d3.scaleLinear().range([height, 0]);
 var parseTime = d3.timeParse('%d.%m.%Y %H:%M:%S');
 
 //set the graph line
-var valueline = d3.line()
-    .x(function(d) { return x(d.datum)})
-    .y(function(d) { return y(d.werte.Tavg_temp); });
+var valueline0 = d3.line()
+    .x(function (d) { console.log(d); return x(d.datum) })
+    .y(function (d) { return y(d.werte.Tavg_vibr); });
+
+    //set the graph line
+var valueline1 = d3.line()
+.x(function (d) { console.log(d); return x(d.datum) })
+.y(function (d) { return y(d.werte.Tavg_vibr); });
 
 
 //appending the svg 'canvas'
-var svg = d3.select("body").append("svg")
+var svg0 = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-         .append("g")
-              .attr("transform",  "translate(" + margin.left + "," + margin.top + ")");
-              
-              
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var svg1 = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 //load the data from url array <files>  
 files.forEach(function (files) {
     promises.push(d3.json(files))
@@ -46,36 +56,50 @@ function visualiseData(data) {
 
     //some test logs
     console.log(data[0][12].datum);
-    console.log(data[0]);  
+    console.log(data);
 
     //formatting the data:
-    data[0].forEach(function(d) {
+    data[3].forEach(function (d) {
         d.datum = parseTime(d.datum);
-        d.werte.Tavg_temp= convertCommaFloats(d.werte.Tavg_temp)
+        d.werte.Tavg_vibr = convertCommaFloats(d.werte.Tavg_vibr)
     });
-    
 
-    x.domain(d3.extent(data[0], function(d) { return d.datum; }));
-    y.domain([0, d3.max(data[0], function(d) { return (d.werte.Tavg_temp); })]);
+
+    x.domain(d3.extent(data[3], function (d) { return d.datum; }));
+    y.domain([0, d3.max(data[3], function (d) { return (d.werte.Tavg_vibr); })]);
 
 
     // Add the valueline path.
-    svg.append("path")
-    .data([data[0]])
-    .attr("class", "line")
-    .attr("d", valueline);
+    svg0.append("path")
+        .data([data[3]])
+        .attr("class", "line")
+        .attr("d", valueline0);
 
     // Add the X Axis
-    svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    svg0.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
     // Add the Y Axis
-    svg.append("g")
+    svg0.append("g")
         .call(d3.axisLeft(y));
 
-    
- 
+    // Add the valueline path.
+    svg1.append("path")
+        .data([data[2]])
+        .attr("class", "line")
+        .attr("d", valueline1);
+
+    // Add the X Axis
+    svg1.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add the Y Axis
+    svg1.append("g")
+        .call(d3.axisLeft(y));
+
+
 };
 
 
@@ -103,7 +127,8 @@ function convertCommaFloats(inpt) {
 */
 
 //dead code for a question
-/*d3.select('#svg_canvas').append('line')
+/*
+d3.select('#svg_canvas').append('line')
     .attr('x1', '0')
     .attr('y1', '595')
     .attr('x2', '1195')
@@ -112,23 +137,4 @@ function convertCommaFloats(inpt) {
 
 
 d3.select('#header_div').append('p').text('hallo')
-
-var newSvg = d3.select('body').append('svg')
-    .attr('width', 200)
-    .attr('height', 200);
-
-    svgContainer= d3.select('g')
-        .selectAll('g')
-        .data(data[0])
-        .enter()
-        .append('g')
-        .attr('class', 'node')
-        .attr('transform', function (d) {
-            return ('translate(' + (data[0].indexOf(d) * 1200 / 310 + 0) + ',' + ((600 - 0.1 * Math.pow(convertCommaFloats(d.werte.Tavg_laut), 2)) + 0) + ')');
-        })
-
-    svgContainer.append('circle')
-        .attr('r', 2)
-        .style('fill', 'blue')
-
 */
