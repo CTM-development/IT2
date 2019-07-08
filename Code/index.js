e6abd10946352ca91d93679947647e3ee815e9d0
CTM-development <45259971+CTM-development@users.noreply.github.com>
2019-07-09 00:02:12 +0200

    first working 'live data' animation testData display

--- Code/index.js
+++ Code/index.js
@@ -4,107 +4,500 @@ var promises = [];
 
 var dataSave;
 
-//set base val variables for later use
-var margin = { top: 20, right: 20, bottom: 30, left: 50 },
-    width = 1200 - margin.left - margin.right,
-    height = 900 - margin.top - margin.bottom;
+//set scale of the svg canvas, change width and height here
 
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+var ticksY = 10;
+var xUpperBound = 1.2;
+var winWidth = window.innerWidth;
+var winHeight = window.innerHeight;
 
+var margin = { top: 20, right: 10, bottom: 30, left: 60 },
+    width = winWidth / 2.2 - margin.left - margin.right,
+    height = winHeight / 2.2 - margin.top - margin.bottom;
+
+
+//custom time parser
 var parseTime = d3.timeParse('%d.%m.%Y %H:%M:%S');
 
-//set the graph line
-var valuelineData = d3.line()
-    .x(function (d) { console.log(d); return x(d.datum) })
-    .y(function (d) { return y(d.werte.Tavg_vibr); });
-
-    //set the graph line
-var valuelineBase = d3.line()
-.x(function (d) { console.log(d); return x(d.datum) })
-.y(function (d) { return y(d.werte.Tavg_vibr); });
 
 
-//appending the svg 'canvas'
-var svg0 = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var svg1 = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-//load the data from url array <files>  
+//load the data from url array 'files'
 files.forEach(function (files) {
     promises.push(d3.json(files))
 });
 
-
 Promise.all(promises).then((result) => receiveData(result), function (error) {
     console.log('Something went wrong!!' + error);
-})
+});
 
+//data was loaded without any errors
 function receiveData(data) {
     console.log('data was received succefuly');
-    visualiseData(data);
+    console.log(data);
+    visualiseData_1(data[0]);
+    visualiseData_2(data[1]);
+    visualiseData_3(data[2]);
+    visualiseData_4(data[3]);
 };
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //actuall visualisation  ation work is done here
-function visualiseData(data) {
+
+
+function visualiseData_1(data) {
+
     dataSave = data;
-    console.log(dataSave);
 
     //some test logs
-    console.log(data[0][12].datum);
-    console.log(data);
+    //console.log(data[0]);
 
-    //formatting the data:
-    data[3].forEach(function (d) {
+
+    //appending the svg 'canvas'
+    var svg = d3.select("#dataVis_1").append("svg")
+        .attr("width", '100%')
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+
+    //formatting the data
+    data.forEach(function (d) {
         d.datum = parseTime(d.datum);
-        d.werte.Tavg_vibr = convertCommaFloats(d.werte.Tavg_vibr)
+        d.werte.DATA = convertCommaFloats(d.werte.Tavg_vibr)
     });
 
+    // setting the ranges for the x and y axes
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
 
-    x.domain(d3.extent(data[3], function (d) { return d.datum; }));
-    y.domain([0, d3.max(data[3], function (d) { return (d.werte.Tavg_vibr); })]);
+
+    //setting the domains for the x and y axes
+    x.domain(d3.extent(data, function (d) { return d.datum; }));
+    y.domain([0, xUpperBound]);
 
 
-    // Add the valueline path.
-    svg0.append("path")
-        .data([data[3]])
-        .attr("class", "line")
-        .attr("d", valuelineData);
+    //custom axes
+    var xAxis = d3.axisBottom(x)
+        .ticks(3)
 
-    // Add the X Axis
-    svg0.append("g")
+    var yAxis = d3.axisLeft(y)
+        .ticks(ticksY);
+
+    //appending the axes to the graph
+    svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        .attr("class", 'axis')
+        .call(xAxis);
 
-    // Add the Y Axis
-    svg0.append("g")
-        .call(d3.axisLeft(y));
 
-    // Add the valueline path.
-    svg1.append("path")
-        .data([data[0]])
+    svg.append("g")
+        .attr("class", 'axis')
+        .call(yAxis);
+
+
+
+    // Add X axis label:
+    svg.append("text")
+        .attr("class", "axisLable")
+        .attr("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height + margin.top + 5)
+        .text("t in min");
+
+    // Y axis label:
+    svg.append("text")
+        .attr("class", "axisLable")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 20)
+        .attr("x", -margin.top + 30)
+        .text("vib")
+
+    // graphline gets constructed here
+    svg.append("path")
+        .data([data])
         .attr("class", "line")
-        .attr("d", valuelineData);
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(d.werte.DATA);
+            }))
 
-    // Add the X Axis
-    svg1.append("g")
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'RPM')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(d.werte.Rpm / 1000);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'temp')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(convertCommaFloats(d.werte.Tavg_temp) / 100);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'laut')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(convertCommaFloats(d.werte.Tavg_laut) / 100);
+            }))
+
+
+
+}
+
+function visualiseData_2(data) {
+
+    dataSave = data;
+
+    //some test logs
+    //console.log(data[0]);
+
+
+    //appending the svg 'canvas'
+    var svg = d3.select("#dataVis_2").append("svg")
+        .attr("width", '100%')
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    //formatting the data
+    data.forEach(function (d) {
+        d.datum = parseTime(d.datum);
+        d.werte.DATA = convertCommaFloats(d.werte.Tavg_vibr)
+    });
+
+    // setting the ranges for the x and y axes
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+
+    //setting the domains for the x and y axes
+    x.domain(d3.extent(data, function (d) { return d.datum; }));
+    y.domain([0, xUpperBound]);
+
+
+
+    //custom axes
+    var xAxis = d3.axisBottom(x)
+        .ticks(3)
+
+    var yAxis = d3.axisLeft(y)
+        .ticks(ticksY);
+
+    //appending the axes to the graph
+    svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // Add the Y Axis
-    svg1.append("g")
-        .call(d3.axisLeft(y));
+        .attr("class", 'axis')
+        .call(xAxis);
 
 
-};
+    svg.append("g")
+        .attr("class", 'axis')
+        .call(yAxis);
+
+    // Add X axis label:
+    svg.append("text")
+        .attr("class", "axisLable")
+        .attr("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height + margin.top + 5)
+        .text("t in min");
+
+    // Y axis label:
+    svg.append("text")
+        .attr("class", "axisLable")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 20)
+        .attr("x", -margin.top + 30)
+        .text("temp in C")
+
+    // graphline gets constructed here
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(d.werte.DATA);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'RPM')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(d.werte.Rpm / 1000);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'temp')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(convertCommaFloats(d.werte.Tavg_temp) / 100);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'laut')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(convertCommaFloats(d.werte.Tavg_laut) / 100);
+            }))
+
+
+
+}
+function visualiseData_3(data) {
+
+    dataSave = data;
+
+    //some test logs
+    //console.log(data[0]);
+
+
+    //appending the svg 'canvas'
+    var svg = d3.select("#dataVis_3").append("svg")
+        .attr("width", '100%')
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+
+    //formatting the data
+    data.forEach(function (d) {
+        d.datum = parseTime(d.datum);
+        d.werte.DATA = convertCommaFloats(d.werte.Tavg_vibr)
+    });
+
+    // setting the ranges for the x and y axes
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+
+    //setting the domains for the x and y axes
+    x.domain(d3.extent(data, function (d) { return d.datum; }));
+    y.domain([0, xUpperBound]);
+
+
+    //custom axes
+    var xAxis = d3.axisBottom(x)
+        .ticks(3)
+
+    var yAxis = d3.axisLeft(y)
+        .ticks(ticksY);
+
+    //appending the axes to the graph
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .attr("class", 'axis')
+        .call(xAxis);
+
+
+    svg.append("g")
+        .attr("class", 'axis')
+        .call(yAxis);
+
+
+
+    // Add X axis label:
+    svg.append("text")
+        .attr("class", "axisLable")
+        .attr("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height + margin.top + 5)
+        .text("t in min");
+
+    // Y axis label:
+    svg.append("text")
+        .attr("class", "axisLable")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 20)
+        .attr("x", -margin.top + 30)
+        .text("vib")
+
+    // graphline gets constructed here
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(d.werte.DATA);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'RPM')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(d.werte.Rpm / 1000);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'temp')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(convertCommaFloats(d.werte.Tavg_temp) / 100);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'laut')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(convertCommaFloats(d.werte.Tavg_laut) / 100);
+            }))
+}
+
+function visualiseData_4(data) {
+
+    dataSave = data;
+
+    //some test logs
+    //console.log(data[0]);
+
+
+    //appending the svg 'canvas'
+    var svg = d3.select("#dataVis_4").append("svg")
+        .attr("width", '100%')
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+
+    //formatting the data
+    data.forEach(function (d) {
+        d.datum = parseTime(d.datum);
+        d.werte.DATA = convertCommaFloats(d.werte.Tavg_vibr)
+    });
+
+    // setting the ranges for the x and y axes
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+
+    //setting the domains for the x and y axes
+    x.domain(d3.extent(data, function (d) { return d.datum; }));
+    y.domain([0, xUpperBound]);
+
+
+    //custom axes
+    var xAxis = d3.axisBottom(x)
+        .ticks(3)
+
+    var yAxis = d3.axisLeft(y)
+        .ticks(ticksY);
+
+    //appending the axes to the graph
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .attr("class", 'axis')
+        .call(xAxis);
+
+
+    svg.append("g")
+        .attr("class", 'axis')
+        .call(yAxis);
+
+
+
+    // Add X axis label:
+    svg.append("text")
+        .attr("class", "axisLable")
+        .attr("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height + margin.top + 5)
+        .text("t in min");
+
+    // Y axis label:
+    svg.append("text")
+        .attr("class", "axisLable")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 20)
+        .attr("x", -margin.top + 30)
+        .text("vib")
+
+    // graphline gets constructed here
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(d.werte.DATA);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'RPM')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(d.werte.Rpm / 1000);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'temp')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(convertCommaFloats(d.werte.Tavg_temp) / 100);
+            }))
+
+    svg.append('path')
+        .data([data])
+        .attr("class", "line")
+        .attr('id', 'laut')
+        .attr("d", d3.line()
+            .x(function (d, i) { return x(d.datum) })
+            .y(function (d, i) {
+                return y(convertCommaFloats(d.werte.Tavg_laut) / 100);
+            }))
+
+
+
+}
+
 
 
 function convertCommaFloats(inpt) {
@@ -118,3 +511,15 @@ function convertCommaFloats(inpt) {
 
 };
 
+
+function computeDimensions(selection) {
+    var node = selection.node();
+
+    if (node instanceof SVGElement) {
+        dimensions = node.getBBox();
+    } else {
+        dimensions = node.getBoundingClientRect();
+    }
+
+    console.log(dimensions);
+}
--- Code/machineX_Graphs.js
+++ Code/machineX_Graphs.js
@@ -1,25 +1,21 @@
 //save the url links to the data download here
-var files = ['http://it2wi1.if-lab.de/rest/mpr_fall1','http://it2wi1.if-lab.de/rest/mpr_fall2'];
+var files = ['http://it2wi1.if-lab.de/rest/mpr_fall4', 'http://it2wi1.if-lab.de/rest/mpr_fall2'];
 var promises = [];
 
 var dataSave;
 
-//set scale of the svg canvas, change width and height here
-var margin = { top: 20, right: 20, bottom: 30, left: 50 },
-    width = 1200 - margin.left - margin.right,
-    height = 900 - margin.top - margin.bottom;
+var winWidth = window.innerWidth;
+var winHeight = window.innerHeight;
+
+var margin = { top: 20, right: 10, bottom: 30, left: 60 },
+    width = winWidth / 2.2 - margin.left - margin.right,
+    height = winHeight / 2.2 - margin.top - margin.bottom;
 
 
 //custom time parser
 var parseTime = d3.timeParse('%d.%m.%Y %H:%M:%S');
 
 
-//appending the svg 'canvas'
-var svg = d3.select("#dataVis").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 //load the data from url array 'files'
@@ -34,8 +30,14 @@ Promise.all(promises).then((result) => receiveData(result), function (error) {
 //data was loaded without any errors
 function receiveData(data) {
     console.log('data was received succefuly');
-    console.log(data);
-    visualiseData(data);
+    
+
+    //formatting the data
+    data[0].forEach(function (d) {
+        d.datum = parseTime(d.datum);
+        d.DATA = convertCommaFloats(d.werte.Tavg_vibr)
+    });
+    visualiseData(data[0]);
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -44,54 +46,78 @@ function receiveData(data) {
 
 function visualiseData(data) {
 
-    dataSave = data;
-
     //some test logs
-    //console.log(data[0]);
+    console.log(data);
 
-    //formatting the data
-    data[0].forEach(function (d) {
-        d.datum = parseTime(d.datum);
-        d.werte.Tavg_vibr = convertCommaFloats(d.werte.Tavg_vibr)
-    });
+    //appending the svg 'canvas'
+    var graph = d3.select("#dataVis").append("svg")
+        .attr("width", '100%')
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 
     // setting the ranges for the x and y axes
-    var x = d3.scaleTime().range([0, width]);
+    var x = d3.scaleLinear().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
 
 
     //setting the domains for the x and y axes
-    x.domain(d3.extent(data[0], function (d) { return d.datum; }));
-    y.domain([0, d3.max(data[0], function (d) { return (d.werte.Tavg_vibr); })]);
+    x.domain([0,10]);
+    y.domain([0, 0.25]);
+
+    //custom axes
+    var xAxis = d3.axisBottom(x)
+        .ticks(10)
+
+    var yAxis = d3.axisLeft(y)
+        .ticks(10);
 
     //appending the axes to the graph
-    svg.append("g")
+    graph.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    svg.append("g")
-        .call(d3.axisLeft(y));
+        .attr("class", 'axis')
+        .call(xAxis);
 
 
+    graph.append("g")
+        .attr("class", 'axis')
+        .call(yAxis);
+
+    var line = d3.line()
+        .x(function (d, i) { 
+            if(i<=0){console.log(d,i)};
+            return x(i); })
+        .y(function (d, i) { return y(d.DATA)});
 
 
-
-
-
-    // graph gets constructed here
-    svg.append("path")
-        .data([data[0]])
+    // graphline gets constructed here
+    graph.append("path")
+        .data([data])
         .attr("class", "line")
-        .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
-        .attr('stroke-width', 1.5)
-        .attr("d", d3.line()
-            .x(function (d, i) { console.log(d,i); return x(d.datum) })
-            .y(function (d) { return y(d.werte.Tavg_vibr); }))
+        .attr("d", line(data));
+
+
+
+    function redrawWithAnimation() {
+        // update with animation
+        graph.selectAll(".line")
+            .data([data]) // set the new data
+            .attr("transform", "translate(0," + x(1) + ")") // set the transform to the right by x(1) pixels (6 for the scale we've set) to hide the new value
+            .attr("d", line) // apply the new data values ... but the new value is hidden at this point off the right of the canvas
+            .transition() // start a transition to bring the new value into view
             
+            .duration(100) // for this demo we want a continual slide so set this to the same as the setInterval amount below
+            .attr("transform", "translate(0," + x(0) + ")"); // animate a slide to the left back to x(0) pixels to reveal the new value
+
+    }
 
 
-
+    setInterval(function () {
+        var v = data.shift();
+        data.push(v);
+        redrawWithAnimation();
+    }, 100)
 
 }
 
